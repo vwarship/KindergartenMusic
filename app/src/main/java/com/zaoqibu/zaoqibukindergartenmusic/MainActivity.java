267c9ec0fcdec1a098f0d5f3c602172323902e41
vwarship <vwarship@163.com>
2014-12-17 04:40:38 +0800

    增加了菜单

--- app/src/main/java/com/zaoqibu/zaoqibukindergartenmusic/MainActivity.java
+++ app/src/main/java/com/zaoqibu/zaoqibukindergartenmusic/MainActivity.java
@@ -73,9 +73,9 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
         return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/zaoqibu/zaoqibukindergartenmusic/TermActivity.java
+++ app/src/main/java/com/zaoqibu/zaoqibukindergartenmusic/TermActivity.java
@@ -215,7 +215,8 @@ public class TermActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_bedtime_paly) {
+
             return true;
         }
         return super.onOptionsItemSelected(item);
