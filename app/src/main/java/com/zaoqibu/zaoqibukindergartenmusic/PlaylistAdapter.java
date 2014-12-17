dc4695f35fb2596af788c242a8128c23ce1c1c7f
vwarship <vwarship@163.com>
2014-12-18 03:03:58 +0800

    为歌曲列表增加了图标，方便儿童查找歌曲。

--- app/src/main/java/com/zaoqibu/zaoqibukindergartenmusic/PlaylistAdapter.java
+++ app/src/main/java/com/zaoqibu/zaoqibukindergartenmusic/PlaylistAdapter.java
@@ -1,6 +1,9 @@
 package com.zaoqibu.zaoqibukindergartenmusic;
 
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +13,10 @@ import android.widget.TextView;
 
 import com.zaoqibu.zaoqibukindergartenmusic.domain.Playlist;
 import com.zaoqibu.zaoqibukindergartenmusic.domain.Sound;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by vwarship on 2014/10/23.
@@ -51,6 +58,16 @@ public class PlaylistAdapter extends BaseAdapter {
         }
 
         Sound sound = player.getPlaylist().getSound(position);
+
+        ImageView icon = (ImageView)view.findViewById(R.id.playlistItemIcon);
+        try {
+            String path = sound.getPath().replace("mp3", "png");
+            InputStream inputStream = context.getAssets().open(path);
+            icon.setImageDrawable(Drawable.createFromStream(inputStream, null));
+        } catch (IOException e) {
+            e.printStackTrace();
+            icon.setImageResource(R.drawable.sound_default_icon);
+        }
 
         TextView name = (TextView)view.findViewById(R.id.playlistItemName);
         name.setText(sound.getName());
